39f0da0a4ab1b6d26f06f2b403fde3c9918f03f7
eswift0112-stack <e.swift0112@gmail.com>
2026-07-20 12:23:07 +0100

    Folder defined af2_pmpnn.sh

--- af2_pmpnn.sh
+++ af2_pmpnn.sh
@@ -5,28 +5,28 @@
 ### EDIT BELOW ###
 ##################
 
-folder_with_pdbs="input_dir"           # where are our input PDBs to start the cycle? This is also where data will end up
+folder_with_pdbs=input_dir           # where are our input PDBs to start the cycle? This is also where data will end up
 
 seqs_per_run=15                         # how many sequences should we make per input structure? Remember a structure will 
                                        # be predicted for each seq here! Time intensive variable
 
-determine_CDRs='martin'                 # this variable is ONLY allowed to be 'structure' OR an antibody numbering scheme
+determine_CDRs=martin                 # this variable is ONLY allowed to be 'structure' OR an antibody numbering scheme
                                        # I have only tested kabat, martin, and chothia for these values. 
 
 ss_near_CDRs=3                         # locate all residues within X Angstroms of the CDRs to group together with as the secondary shell. 
 
 simple_grab=true                       # When doing the distance calculations for the CDRs, do a simple grab or do an intelligent grab of the neighbors
 
-output_dir="output_dir"                # temp output directory where less important (mediary ProteinMPNN) data ends up
+output_dir=output_dir                # temp output directory where less important (mediary ProteinMPNN) data ends up
 
-to_design="framework"                  # what are we designing? ONLY 3 options: "framework", "loops", and "lss". lss = loops and secondary shell,
+to_design=framework                  # what are we designing? ONLY 3 options: "framework", "loops", and "lss". lss = loops and secondary shell,
                                        # or the Vernier zone region. The part of the scFv thats 'in between' the CDR loops and the framework.
 
-linker_seq=""                          # specify linker sequence: if none is specified and left blank, automatically try to detect
+linker_seq=GGGGSGGGGSGGGGS                          # specify linker sequence: if none is specified and left blank, automatically try to detect
                                        # linker sequences from the following list: 
 				       # ['GGGGSGGGGSGGGGS', 'GGSGGSGGSGGSGGS', 'GSGSGSGSGSGSGS', 'GGGSGGGSGGGSGGS', 'GGGSGGGSGGGS']
 
-PMPNN='path/to/my/ProteinMPNN' # the global path to where the ProteinMPNN github was cloned to locally
+PMPNN=home/c21094846/Proteinmpnn # the global path to where the ProteinMPNN github was cloned to locally
 
 # How was this used to generate data?   nohup bash af2_pmpnn.sh &
 
