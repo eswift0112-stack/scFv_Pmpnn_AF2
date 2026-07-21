2d5b8d2e89014b2d5533ceae8806ad4106bc98aa
eswift0112-stack <e.swift0112@gmail.com>
2026-07-21 09:48:08 +0100

    Update af2_pmpnn.sh

--- af2_pmpnn.sh
+++ af2_pmpnn.sh
@@ -5,7 +5,7 @@
 ### EDIT BELOW ###
 ##################
 
-folder_with_pdbs=input_dir           # where are our input PDBs to start the cycle? This is also where data will end up
+folder_with_pdbs=scFv_Pmpnn_AF2/input_dir           # where are our input PDBs to start the cycle? This is also where data will end up
 
 seqs_per_run=15                         # how many sequences should we make per input structure? Remember a structure will 
                                        # be predicted for each seq here! Time intensive variable
@@ -17,7 +17,7 @@ ss_near_CDRs=3                         # locate all residues within X Angstroms 
 
 simple_grab=true                       # When doing the distance calculations for the CDRs, do a simple grab or do an intelligent grab of the neighbors
 
-output_dir=output_dir                # temp output directory where less important (mediary ProteinMPNN) data ends up
+output_dir=scFv_Pmpnn_AF2/output_dir                # temp output directory where less important (mediary ProteinMPNN) data ends up
 
 to_design=framework                  # what are we designing? ONLY 3 options: "framework", "loops", and "lss". lss = loops and secondary shell,
                                        # or the Vernier zone region. The part of the scFv thats 'in between' the CDR loops and the framework.
@@ -26,7 +26,7 @@ linker_seq=GGGGSGGGGSGGGGS                          # specify linker sequence: i
                                        # linker sequences from the following list: 
 				       # ['GGGGSGGGGSGGGGS', 'GGSGGSGGSGGSGGS', 'GSGSGSGSGSGSGS', 'GGGSGGGSGGGSGGS', 'GGGSGGGSGGGS']
 
-PMPNN=home/c21094846/ProteinMPNN # the global path to where the ProteinMPNN github was cloned to locally
+PMPNN=ProteinMPNN # the global path to where the ProteinMPNN github was cloned to locally
 
 # How was this used to generate data?   nohup bash af2_pmpnn.sh &
 
